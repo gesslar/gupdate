f4a11f8d45ea38a6c7f8d7b01017e164e6c87543
GitHub Action <action@github.com>
2024-11-25 06:21:42 +0000

    Auto-build GitHub Action

--- action/index.js
+++ action/index.js
@@ -27788,6 +27788,8 @@ try {
     .split('\n')
     .filter(Boolean);
 
+  core.info(`Tags: ${JSON.stringify(tags)}`);
+
   // Use compare-versions to find the latest tag
   const latestTag = tags.reduce((latest, current) => {
     const currentVer = current.replace(/^v/, '');
