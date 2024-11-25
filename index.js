4f3d7befa24177d59fe2f22dbe2ac28b6ca91ad0
gesslar <karahd@gmail.com>
2024-11-25 01:17:21 -0500

    was using the wrong function 🙄

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ try {
 
   const latestVersion = latestTag ? latestTag.replace(/^v/, '') : null;
 
-  core.log(`Current version: ${currentVersion}, Latest version: ${latestVersion}`);
+  core.info(`Current version: ${currentVersion}, Latest version: ${latestVersion}`);
 
   // Compare versions and set output
   if (!latestVersion) {
