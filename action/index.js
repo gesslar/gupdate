d9ec51cc20eccd63b7d766b37ac947a83ab4bb7d
GitHub Action <action@github.com>
2024-11-25 06:20:13 +0000

    Auto-build GitHub Action

--- action/index.js
+++ action/index.js
@@ -27801,6 +27801,7 @@ try {
     }
   }, null);
 
+  core.info(`Latest tag: ${JSON.stringify(latestTag)}`);
   const latestVersion = latestTag ? latestTag.replace(/^v/, '') : null;
 
   core.info(`Current version: ${currentVersion}, Latest version: ${latestVersion}`);
